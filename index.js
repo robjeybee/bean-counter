5b9f62ce31ae12f1d71a54c789ce8031e3ed5a01
Rob B <rob@rjrobbie.dev>
2024-01-02 19:35:42 +0000

    added save and reset buttons

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 let count = 0
 
+let countEl = document.getElementById("count-el")
+
+let saveEl = document.getElementById("save-el")
+
+let resetEl = document.getElementById("reset-btn")
+
 const increment = () => {
-    count = count + 1
-    document.getElementById("count-el").innerHTML = count
+    count += 1
+    countEl.innerHTML = count
     console.log(count)
+}
+
+const save = () => {
+    let countString = count + " - "
+    saveEl.innerHTML += countString
+    countEl.innerHTML = 0
+    count = 0
+}
+
+const reset = () => {
+    countEl.innerHTML = 0
+    count = 0
+    saveEl.innerHTML = ""
 }
 
 
